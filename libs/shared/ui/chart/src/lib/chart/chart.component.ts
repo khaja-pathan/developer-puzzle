831bffbcc3a1f14dadbfd5662348f5bdb98f9b27
Khazarahamtullah Pattan <kpattan@pkrmsezdc.com>
2019-12-23 17:06:16 +0530

    added custom dates

--- libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -14,6 +14,9 @@ import { Observable } from 'rxjs';
 })
 export class ChartComponent implements OnInit {
   @Input() data$: Observable<any>;
+  @Input() fromDate;
+  @Input() toDate;
+
   chartData: any;
 
   chart: {
@@ -34,6 +37,21 @@ export class ChartComponent implements OnInit {
       options: { title: `Stock price`, width: '600', height: '400' }
     };
 
-    this.data$.subscribe(newData => (this.chartData = newData));
+    this.data$.subscribe(newData => {
+      if (this.fromDate && this.toDate) {
+        let fromDate = null;
+        let toDate = null;
+        fromDate = `${this.fromDate.getFullYear()}-${this.fromDate.getMonth() + 1}-${this.fromDate.getDate()}`;
+        toDate = `${this.toDate.getFullYear()}-${this.toDate.getMonth() + 1}-${this.toDate.getDate()}`;
+        this.chartData = newData.filter((value) => {
+          if (fromDate <= value[0] && value[0] <= toDate) {
+            return true;
+          }
+          return false;
+        });
+      } else {
+        this.chartData = newData;
+      }
+    });
   }
 }
